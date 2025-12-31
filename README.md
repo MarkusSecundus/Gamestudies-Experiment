c0a7cc99552f71cecb8f69b699af1938bbed9ac8
MarkusSecundus <kubahronik.g@gmail.com>
2025-12-31 19:07:02 +0100

    fix a blink during fading + other minor improvements

--- README.md
+++ README.md
@@ -7,3 +7,4 @@ Requires Godot 4.5.1
 Used third-party assets:
  - default typewriter sounds from [Dialogic](https://github.com/coppolaemilio/dialogic)
  - [Vetaran typewriter font](https://www.1001fonts.com/veteran-typewriter-font.html) by [Magique Fonts](https://www.1001fonts.com/users/magique/)
+ - thunder1: https://freesound.org/people/nednednerb/sounds/38244/
---
+++ scripts/dialog_custom_actions/DialogAction_Fader.gd
@@ -0,0 +1,20 @@
+extends IDialogAction
+
+@export var start_alpha : float
+@export var end_alpha : float
+@export var duration_seconds : float
+@export var ease : Tween.EaseType = Tween.EaseType.EASE_IN_OUT
+@export var transition : Tween.TransitionType = Tween.TransitionType.TRANS_LINEAR
+
+func do_perform(ctx: DialogContext, on_finished: Callable)->void:
+	var tw := create_tween()
+	var fader : Control = Fader
+	var final_color = fader.modulate
+	final_color.a = end_alpha
+	fader.modulate.a = start_alpha
+	var tweener := tw.tween_property(fader, "modulate", final_color, duration_seconds)
+	tweener.set_ease(ease)
+	tweener.set_trans(transition)
+	await tw.finished
+	
+	_default_perform(ctx, on_finished)
