8feedd29f372a4045580cce325683dea4c2a8f2a
MarkusSecundus <kubahronik.g@gmail.com>
2026-01-06 13:59:49 +0100

    add coin sounds

--- scripts/dialog_system/IDialogAction.gd
+++ scripts/dialog_system/IDialogAction.gd
@@ -5,9 +5,15 @@ extends SomeDialogContainer
 
 func do_perform(ctx: DialogContext, on_finished: Callable)->void:
 	_default_perform(ctx, on_finished)
+	
 
 func _default_perform(_ctx: DialogContext, on_finished: Callable)->void:
 	on_finished.call(null)
 
 func _get_first_child()->IDialogAction:
 	return NodeUtils.get_child_of_type(self, IDialogAction)
+
+func run_dialog()->void:
+	var system := NodeUtils.get_ancestor_of_type(self, DialogSystem) as DialogSystem
+	assert(system)
+	system.do_run(self)
