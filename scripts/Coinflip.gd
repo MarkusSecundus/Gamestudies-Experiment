2abf23e3ec0432ec457ef773058994dd509432c7
MarkusSecundus <kubahronik.g@gmail.com>
2026-01-25 23:11:51 +0100

    t.subj. now gets assigned test group and random ID

---
+++ scripts/Coinflip.gd
@@ -0,0 +1,48 @@
+extends Node
+
+var id : String
+var is_game : bool
+
+var chosen_parent : Node2D:
+	get: return $GameFirst if is_game else $QuestionnaireFirst
+
+func _ready() -> void:
+	id = load_random_id().strip_edges()
+	is_game = load_is_game_first()
+	$QuestionnaireFirst.visible = false
+	$GameFirst.visible = false
+	chosen_parent.visible = true
+	var lbl = chosen_parent.get_node("Label")
+	lbl.text = lbl.text.format([id])
+	EyePiedestal.write_record({"type": "user_id", "id": id})
+
+
+func load_random_id()->String:
+	return get_persistent("user://test_subject_name.txt", generate_random_id)
+	
+func load_is_game_first()->bool:
+	var str := get_persistent("user://cointoss.txt", randi)
+	var num := int(str)
+	return (num & 2)
+
+func generate_random_id()->int:
+	return ResourceUID.create_id() + randi()
+
+func copy_id_to_clipboard()->void:
+	DisplayServer.clipboard_set(id)
+
+
+static func get_persistent(path : String, supplier : Callable)->String:
+	var fread := FileAccess.open(path, FileAccess.READ)
+	if fread && fread.is_open():
+		var ret := fread.get_as_text()
+		fread.close()
+		return ret
+	
+	var ret := "%s"%supplier.call()
+	var fwrite := FileAccess.open(path, FileAccess.WRITE)
+	if fwrite and fwrite.is_open():
+		fwrite.store_line(ret)
+		fwrite.flush()
+		fwrite.close()
+	return ret
--- scripts/dialog_system/DialogWaitForSignal.gd
+++ scripts/dialog_system/DialogWaitForSignal.gd
@@ -9,6 +9,9 @@ func _ready() -> void:
 	if provider: provider.the_signal.connect(do_complete.emit)
 	do_complete.connect(func(): print("complete signal: {0}".format([self.name])))
 
+func do_complete_func()->void:
+	do_complete.emit()
+
 func do_perform(ctx: DialogContext, on_finished: Callable)->void:
 	await do_complete
 	_default_perform(ctx, on_finished)
